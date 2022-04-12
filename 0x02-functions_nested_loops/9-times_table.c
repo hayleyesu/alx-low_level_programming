6df1dc856dcba40e7357d00f1ff67c9d7d94ac2d
hayleyesu <101431878+hayleyesu@users.noreply.github.com>
2022-04-13 02:15:42 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,29 +4,28 @@
   * Description: It prints 9 times table starting with 0.
   * Return: nothing.
   */ 
-
 void times_table(void)
 {
-	int i, j;
+ int i, j;
 
-	for (i = 0; i <= 9; i++)
+  for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+	 for (j = 0; j <= 9; j++)
 		{
-			int mul;
+		 int mul;
 
-			mul = j * i;
+		  mul = j * i;
 
-			if ((mul / 10) == 0)
+		   if ((mul / 10) == 0)
 			{
 				if (j != 0)
 					_putchar(' ');
-				_putchar(mul + '0');
-				if (j == 9)
+				        _putchar(mul + '0');
+				         if (j == 9)
 					continue;
 
-				_putchar(',');
-				_putchar(' ');
+				        _putchar(',');
+				        _putchar(' ');
 			}
 			else
 			{
