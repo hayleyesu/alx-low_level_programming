f627a18f8866929e81ddd432a1eff38d8d0c8dc6
hayleyesu <haylajul03@gmail.com>
2022-04-18 12:45:53 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		s++;
 		count++;
 	}
-	while(count > 0)
+	while (count > 0)
 		{
 			s--;
 			_putchar(*s);
