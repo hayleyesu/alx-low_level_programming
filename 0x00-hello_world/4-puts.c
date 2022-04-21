818ba8655e69994bd9be2f5fab6976febe4215b4
hayleyesu <haylajul03@gmail.com>
2022-04-21 04:03:32 -0700

    C program that prints text using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * main - Entry point
- * Return : 0
+ *
+ * Return : 0 after excuting a function
  */
 
 int main(void)
 {
-	puts("\"Programming is like biulding a multilingual puzzle");
+	puts("Programming is like biulding a multilingual puzzle");
+	puts('\n');
 	return (0);
 }
