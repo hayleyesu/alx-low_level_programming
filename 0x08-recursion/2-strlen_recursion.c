ea3d2ddb78f2569943904d50fd6fd96af45552bd
hayleyesu <haylajul03@gmail.com>
2022-04-27 13:02:29 -0700

    print string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,10 @@
 int _strlen_recursion(char *s)
 {
 	int len = 0;
-	len = len + 1;
 
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
+		len = 1 + _strlen_recursion(s + 1);
 	}
 	return (len);
 }
