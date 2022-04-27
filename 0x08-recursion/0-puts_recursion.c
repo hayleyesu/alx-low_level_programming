e1f4eab36b59841a97014b2351aaae4f67af0a71
hayleyesu <haylajul03@gmail.com>
2022-04-27 04:40:36 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string
+ * Return : nothing
+ */
+void _puts_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		_putchar('\n')
+	}
+	printf("%s",_puts_recursion());
+		s++;
+}
