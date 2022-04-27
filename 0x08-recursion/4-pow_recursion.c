ac62f7658e87163eed6353ff1722b7574a8b23f8
hayleyesu <haylajul03@gmail.com>
2022-04-27 13:56:40 -0700

    return power of a given number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _por_recursion - return x the power of  y
+ * _pow_recursion - return x the power of  y
  * @x: base input number
  * @y: power input number
  * Return: x rise y
@@ -12,9 +12,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else if (x == 0)
+	else if (y == 0)
 	{
 		return (1);
 	}
-	return (x * _pow_recursion(x, (y-1)));
+	return (x * _pow_recursion(x, (y - 1)));
 }
