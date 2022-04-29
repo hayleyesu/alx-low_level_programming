c9fbb5242d496d8e582f7944640d184cec229492
hayleyesu <haylajul03@gmail.com>
2022-04-29 12:09:16 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 /**
  * main - function
  * @argc: argument counter
- * @argc :array of arguments
+ * @argc: array of arguments
  * return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
+	
 	return (0);
 }
