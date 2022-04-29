1a4015b0f0a22dd7bce3cff6c3cff77e00186314
hayleyesu <haylajul03@gmail.com>
2022-04-29 12:52:03 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: arguments array
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n",argc);
+}
