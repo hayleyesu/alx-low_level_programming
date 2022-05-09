6497727df496abd147856bab2935a5d24f01dbb2
hayleyesu <101431878+hayleyesu@users.noreply.github.com>
2022-05-10 00:21:27 +0300

    update task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-#define ABS(x) (x >= 0 ? (x) : (-x))
-#endif
+#ifndef ABS_VALUE
+#define ABS_VALUE
+
+#define ABS(x) ((x) < (0) ? ((-1) * (x)) : (x))
+
+#endif /*#ifndef ABS_VALUE*/
