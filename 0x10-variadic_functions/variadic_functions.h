64657f8763d14bdc96f3986d21b2f0214a0a3e08
hayleyesu <101431878+hayleyesu@users.noreply.github.com>
2022-05-12 17:10:43 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,28 @@
-#ifndef variadic_functions_h
-#define variadic_functions_h
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * Function prototypes
+ */
+
+/*
+ * struct check - paramenters to check against
+ * @c: type
+ * @f: the function to send it to
+ */
+typedef struct check
+{
+	char *chk;
+	void (*f)(va_list list);
+} check_t;
+
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
 
 #endif
